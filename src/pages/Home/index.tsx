b2cb6c64b176582319cbcbccec88bc349ff3093f
Tiago Barros <tbarros1337@hotmail.com>
2021-10-20 18:04:06 -0300

    feat: Adds svg to ackground

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -8,7 +8,11 @@ export const Home = (): JSX.Element => {
   const { user } = useAuth();
 
   return (
-    <main className={styles.contentWrapper}>
+    <main
+      className={`${styles.contentWrapper} ${
+        !!user ? styles.contentSigned : ''
+      }`}
+    >
       <MessageList />
       {!!user ? <SendMessageForm /> : <LoginBox />}
     </main>
